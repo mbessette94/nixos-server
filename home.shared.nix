32a1b701f5499e163da1ee5f80c5a419fbebf22f
Matthew Bessette <blade30912@gmail.com>
2026-07-29 01:17:13 -0400

    Fixing some weird typing issues

--- home.shared.nix
+++ home.shared.nix
@@ -2,8 +2,8 @@
 
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
+    # enableCompletion = true;
+    # autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
     shellAliases = {
