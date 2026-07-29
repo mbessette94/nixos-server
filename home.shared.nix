16bb79a73a02936267bb3abaa32b11f30b1eb1fc
Matthew Bessette <blade30912@gmail.com>
2026-07-29 01:16:06 -0400

    drop starship

--- home.shared.nix
+++ home.shared.nix
@@ -18,47 +18,6 @@
     nerd-fonts.jetbrains-mono
   ];
 
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-
-    # Optional custom settings (translates to ~/.config/starship.toml)
-    settings = {
-      # Line 1: ➜ folder git:(branch) [status]
-      format = "$character$directory$git_branch";
-
-      # Show current directory name only
-      directory = {
-        style = "bold cyan";
-        truncation_length = 1;
-        truncate_to_repo = false;
-      };
-
-      # git:(branch) styling
-      git_branch = {
-        format = "[git:\\([$branch](red)\\)](bold blue) ";
-      };
-
-      # Bottom line prompt arrow
-      character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol = "[➜](bold red)";
-        vicmd_symbol = "[➜](bold yellow)";
-      };
-
-      git_status = {
-        disabled = false;
-      };
-
-      # Nix shell indicator module configuration
-      nix_shell = {
-        symbol = "❄️ ";
-        format = "via [$symbol$state]($style) ";
-      };
-    };
-  };
-
-
   # State version baseline
   home.stateVersion = "24.05";
 }
